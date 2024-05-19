853bf7015805959a346263b6110f80a585895412
luizamaniaci <luiza_falci@hotmail.com>
2024-05-18 21:07:12 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  capturedImage: string = '';
+
+  constructor() {}
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    
+    if (image.dataUrl)
+      this.capturedImage = image.dataUrl;
+  }
+}
